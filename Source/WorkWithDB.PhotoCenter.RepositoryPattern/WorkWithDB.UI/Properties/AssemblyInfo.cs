f449d3703ea2bca7954869ec04dbacc8cc2726eb
Danylo <goddan333@gmail.com>
2016-02-29 13:08:28 +0200

    Add strong name, rules - fxCope, styleCop, add nuget packages

--- Source/WorkWithDB.PhotoCenter.RepositoryPattern/WorkWithDB.UI/Properties/AssemblyInfo.cs
+++ Source/WorkWithDB.PhotoCenter.RepositoryPattern/WorkWithDB.UI/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -20,7 +21,7 @@ using System.Windows;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
+[assembly: CLSCompliant(true)]
 //In order to begin building localizable applications, set 
 //<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
 //inside a <PropertyGroup>.  For example, if you are using US english
@@ -53,3 +54,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
